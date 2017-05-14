49ed0275a11d551d03740cfe4fe2f395e258f1df
jimmy820904 <jimmy820904@gmail.com>
2017-05-14 16:02:43 +0800

    定位

--- app/src/main/java/team/prophet/a3104/safe_area/MapsActivity.java
+++ app/src/main/java/team/prophet/a3104/safe_area/MapsActivity.java
@@ -1,8 +1,20 @@
 package team.prophet.a3104.safe_area;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -17,19 +29,52 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
+    private Button b;
+    private TextView t;
+    private LocationManager locationManager;
+    private LocationListener listener;
+    public double x,y;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        t = (TextView) findViewById(R.id.textView);
+        b = (Button) findViewById(R.id.button);
 
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                x = location.getLongitude();
+                y = location.getLatitude();
+                t.append("\n " + location.getLongitude() + " " + location.getLatitude());
+            }
 
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+        configure_button();
     }
 
 
@@ -150,4 +195,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(taichung).title("台中市").snippet("中區建國路周邊\n北區雙十路二段周邊\n東、南區復興路段\n南屯區文心森林公園周邊\n北屯區北屯市場周邊\n西屯區西屯路二段\n豐原區葫蘆墩公園周邊\n烏日區高鐵停車場周邊\n大里區大明、永隆路段週邊\n大甲區經國路、北堤東路口周遭\n太平區育才路與育英街口\n清水區高美路段\n石岡區石岡國中周邊"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(taichung,8));
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 10:
+                configure_button();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void configure_button(){
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                        ,10);
+            }
+            return;
+        }
+        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //noinspection MissingPermission
+                locationManager.requestLocationUpdates("gps", 5000, 0, listener);
+                LatLng now = new LatLng(y,x);
+                mMap.addMarker(new MarkerOptions().position(now));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(now,8));
+            }
+        });
+    }
+
 }
